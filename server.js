dcca612497147b8074cb89ac3af3a98dcfef545d
pedroLSoares <pedro.levada@mercadolivre.com>
2023-04-05 20:39:43 -0300

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+
+const hostname = "127.0.0.1";
+const port = 8080;
+
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
+
+app.get("/error", function (req, res) {
+  res.statusCode = 500;
+  app.emit("error", new Error("teste"));
+});
+
+app.on("error", (err) => console.log(err));
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
